38326948e20c4d14e8ddebd6b8f175bb5c5d9415
wondger <wondger@gmail.com>
2013-01-04 21:40:14 +0800

    onRestart

--- src/com/fedtool/together_android/MainActivity.java
+++ src/com/fedtool/together_android/MainActivity.java
@@ -8,8 +8,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings.Secure;
-import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -49,6 +47,17 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    @Override
+    protected void onRestart() {
+
+        // TODO Auto-generated method stub
+        super.onRestart();
+        Intent i = new Intent(this, MainActivity.class);  //your class
+        startActivity(i);
+        finish();
+
     }
     
     public void getJoinedActivity(String userId) {
